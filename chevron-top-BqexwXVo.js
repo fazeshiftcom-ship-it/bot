a20f2f6d64a886ba4d52a4f02a29986a2e1f9c10
fazeshiftcom-ship-it <fazeshiftcom@gmail.com>
2026-03-03 11:36:06 +0100

    Add files via upload

---
+++ chevron-top-BqexwXVo.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-DUIumCr9.js";import"./index-DJ1iytiU.js";import"./events-CaNHmX3q.js";import"./index.es-CHMSEdjn.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronTopSvg};
